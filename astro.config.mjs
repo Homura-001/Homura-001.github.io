d67b8834b100d018285126b48e50d5374189ae57
Homura-001 <leihuoda@gmail.com>
2025-07-30 11:50:45 +0800

    更新博客配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://github.com/Homura-001/Homura-001.github.io",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
